a72cc63d158d771f84a9461dcd27448d90975b70
Ryan Balinger <rballinger5@gmail.com>
2015-07-14 22:50:11 -0400

    added tutorial part 2 code with code correction 'drawab_launcher -> mipmap/ic_launcher' due to new hierarchy in Android Studio 1.2.1.1

--- OpenGLAndroid2DTutorial/app/src/main/java/com/example/ryan/openglandroid2dtutorial/MainActivity.java
+++ OpenGLAndroid2DTutorial/app/src/main/java/com/example/ryan/openglandroid2dtutorial/MainActivity.java
@@ -1,38 +1,53 @@
 package com.example.ryan.openglandroid2dtutorial;
 
-import android.support.v7.app.ActionBarActivity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
+    // Our OpenGL Surfaceview
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Turn off the window's title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Super
         super.onCreate(savedInstanceState);
+
+        // Fullscreen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // We create our Surfaceview for our OpenGL here.
+        glSurfaceView = new GLSurf(this);
+
+        // Set our view.
         setContentView(R.layout.activity_main);
+
+        // Retrieve our Relative layout from our main layout we just set to our view.
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.gamelayout);
+
+        // Attach our surfaceview to our relative layout from our main layout.
+        RelativeLayout.LayoutParams glParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        layout.addView(glSurfaceView, glParams);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
     }
+
 }
